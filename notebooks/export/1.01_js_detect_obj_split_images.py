a79561726fb417f5650670854843e589dbef00f8
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2025-01-23 21:57:57 -0600

    running img split notebook

--- notebooks/export/1.01_js_detect_obj_split_images.py
+++ notebooks/export/1.01_js_detect_obj_split_images.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import os
 import requests
 import cv2
+import json
+import base64
+import uuid
 from dotenv import load_dotenv
+from urllib.request import Request, urlopen
+from datetime import datetime
+from PIL import Image
+from tqdm.notebook import tqdm
 
 # Load environment variables
 load_dotenv(".env.secrets.15spp_zoom_level_validation_models")
@@ -22,133 +29,170 @@ DIR_TRAIN_TEST = os.getenv("DIR_TRAIN_TEST")
 SPECIES = os.getenv("SPECIES")
 
 IMAGE_FORMAT = os.getenv("IMAGE_FORMAT")
+DETECTOR_API_KEY = os.getenv("DETECTOR_API_KEY")
+
+RUN_LOG = os.getenv("RUN_LOG")
+ERR_LOG = os.getenv("ERR_LOG")
 
 
-# In[ ]:
+# In[2]:
+
+
+def run_log(message, init=False):
+    os.makedirs(os.path.dirname(RUN_LOG), exist_ok=True)
+    with open(RUN_LOG, ("w" if init else "a")) as f:
+        f.write(f"{datetime.now()}: {message}\n")
+
+
+# In[3]:
+
+
+def err_log(message, init=False):
+    os.makedirs(os.path.dirname(ERR_LOG), exist_ok=True)
+    with open(ERR_LOG, ("w" if init else "a")) as f:
+        f.write(f"{datetime.now()}: {message}\n")
+
+
+# In[4]:
 
 
 # Function to save individual seed images
-def save_cropped_seeds(image_path, seeds_bboxes, output_folder, file_prefix):
+def save_cropped_seeds_cv2(image_path, seeds_bboxes, output_folder, file_prefix):
     image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
     for idx, bbox in enumerate(seeds_bboxes):
-        x_min, y_min, x_max, y_max = bbox
+        # print(bbox['box'])
+        topX = bbox["box"]["topX"]
+        topY = bbox["box"]["topY"]
+        bottomX = bbox["box"]["bottomX"]
+        bottomY = bbox["box"]["bottomY"]
+
+        # print(topX, topY, bottomX, bottomY)
+        x_min = int(topX * image.shape[1])
+        x_max = int(bottomX * image.shape[1])
+        y_min = int(topY * image.shape[0])
+        y_max = int(bottomY * image.shape[0])
+
+        # print(x_max, x_min, y_max, y_min)
         cropped_image = image[y_min:y_max, x_min:x_max]
+
+        cropped_image
+
         cropped_filename = f"{file_prefix}_seed{idx}.tiff"
         cropped_filepath = os.path.join(output_folder, cropped_filename)
+
+        os.makedirs(output_folder, exist_ok=True)
+        print(f"Saving cropped image to {cropped_filepath}")
         cv2.imwrite(cropped_filepath, cropped_image)
 
 
-# In[ ]:
+# In[5]:
+
+
+def save_cropped_seeds_pil(image_path, seeds_bboxes, output_folder, file_prefix):
+    image = Image.open(image_path)
+    for idx, bbox in enumerate(seeds_bboxes):
+        # print(bbox['box'])
+        topX = bbox["box"]["topX"]
+        topY = bbox["box"]["topY"]
+        bottomX = bbox["box"]["bottomX"]
+        bottomY = bbox["box"]["bottomY"]
+
+        # print(topX, topY, bottomX, bottomY)
+        x_min = int(topX * image.size[0])
+        x_max = int(bottomX * image.size[0])
+        y_min = int(topY * image.size[1])
+        y_max = int(bottomY * image.size[1])
+
+        # print(x_max, x_min, y_max, y_min)
+        cropped_image = image.crop((x_min, y_min, x_max, y_max))
+
+        cropped_filename = f"{file_prefix}_seed{idx}.tiff"
+        cropped_filepath = os.path.join(output_folder, cropped_filename)
+
+        os.makedirs(output_folder, exist_ok=True)
+        run_log(f"Cropped image : {cropped_filepath}")
+        cropped_image.save(cropped_filepath)
+
+
+# In[6]:
 
 
 # Function to call the model REST endpoint
+
+
 def get_seed_detections(image_path):
     with open(image_path, "rb") as image_file:
-        response = requests.post(
-            url=SEED_DETECTOR_URL,  # Replace with your REST endpoint URL
-            files={"file": (os.path.basename(image_path), image_file)},
-            headers={"Content-Type": "multipart/form-data"},
-        )
-        response.raise_for_status()
-        return response.json()["predictions"]
+        image_string = base64.b64encode(image_file.read())
+
+        data = {
+            "input_data": {
+                "columns": ["image"],
+                "index": [0],
+                "data": [image_string.decode("utf-8")],
+            }
+        }
+        body = str.encode(json.dumps(data))
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": ("Bearer " + DETECTOR_API_KEY),
+        }
+        req = Request(SEED_DETECTOR_URL, body, headers, method="POST")
+        # req = Request("http://192.168.x.x:12380/score", body, headers, method="POST")
+        response = urlopen(req)
+
+        result = response.read()
+        result_object = [json.loads(result.decode("utf8"))]
+        return result_object[0]["boxes"]
 
 
 # In[ ]:
 
 
-def process_folder(folder_path, output_folder, logpath):
+def process_folder(folder_path, output_folder, progress_bar):
     for item in os.listdir(folder_path):
-        print(item)
+        # print(item)
         item_path = os.path.join(folder_path, item)
         if os.path.isdir(item_path):
             output_sub_folder = os.path.join(output_folder, item)
-            process_folder(item_path, output_sub_folder)
+            process_folder(item_path, output_sub_folder, progress_bar)
         else:
             if item.endswith(IMAGE_FORMAT):
                 image_path = item_path
+                # print("Processing : " + image_path)
+                run_log(f"Processing : {image_path}")
                 try:
                     seed_detections = get_seed_detections(image_path)
+                    # print(seed_detections[0]['boxes'])
                     if seed_detections:
-                        save_cropped_seeds(
+                        save_cropped_seeds_pil(
                             image_path,
                             seed_detections,
                             output_folder,
-                            image_path.split(".")[0],
+                            # image_path.split(".")[0],
+                            uuid.uuid4(),
                         )
                     else:
                         # error_log.append(image_path)
                         raise Exception("Error processing image " + image_path)
+                    progress_bar.update(1)
+                    # run_log(f"Completed : {completed_files}/{total_files}")
                 except Exception as e:
-                    with open(logpath, "a") as error_file:
-                        error_file.write(str(e) + "\n")
+                    err_log(f"Error processing image {image_path} : {e}")
+                    print(e)
+                    exit(1)
+
 
 def main():
-    process_folder(DIR_RAW_IMAGES, DIR_OUTPUT_IMAGES, "error_log.txt")
+    run_log("Starting seed detection process", init=True)
+    err_log("Starting seed detection process", init=True)
+    file_array = [len(files) for r, d, files in os.walk(DIR_RAW_IMAGES)]
+    total_files = sum(file_array)
+    progress_bar = tqdm(total=total_files)
+    process_folder(DIR_RAW_IMAGES, DIR_OUTPUT_IMAGES, progress_bar)
+    run_log("Seed detection process completed")
+    err_log("Seed detection process completed")
+
 
 main()
-
-
-# In[ ]:
-
-
-# # Main script
-# def main(data_folder, output_folder):
-#     error_log = []
-#     for label_folder in os.listdir(data_folder):
-#         print(label_folder)
-#         label_folder_path = os.path.join(data_folder, label_folder)
-#         if os.path.isdir(label_folder_path):
-#             for image_file in os.listdir(label_folder_path):
-#                 if image_file.endswith(IMAGE_FORMAT):
-#                     image_path = os.path.join(label_folder_path, image_file)
-#                     try:
-#                         seed_detections = get_seed_detections(image_path)
-#                         if seed_detections:
-#                             save_cropped_seeds(
-#                                 image_path,
-#                                 seed_detections,
-#                                 output_folder,
-#                                 image_file.split(".")[0],
-#                             )
-#                         else:
-#                             error_log.append(image_path)
-#                     except requests.RequestException as e:
-#                         error_log.append(image_path)
-#                         print(f"Request failed for {image_path}: {e}")
-
-#     # Write error log
-#     with open("error.txt", "w") as error_file:
-#         for error_image in error_log:
-#             error_file.write(error_image + "\n")
-
-# main(DIR_RAW_IMAGES, DIR_OUTPUT_IMAGES)
-
-# Replace 'data_folder' and 'output_folder' with your actual folders
-
-
-# In[ ]:
-
-
-# def main(source, subfolders, dest):
-#     error_log = []
-#     subfolder_combinations = []
-#     for i in range(len(subfolders[0])):
-#         for j in range(len(subfolders[1])):
-#             for k in range(len(subfolders[2])):
-#                 subfolder_combinations.append( source + "/" + subfolders[0][i] + "/" +subfolders[1][j] + "/" +subfolders[2][k])
-
-#     # Write error log
-#     with open("error.txt", "w") as error_file:
-#         for error_image in error_log:
-#             error_file.write(error_image + "\n")
-
-# main(
-#     DIR_RAW_IMAGES, [DIR_ZOOM_LVL, ["Training", "Testing"], SPECIES], DIR_OUTPUT_IMAGES
-# )
-
-
-# In[ ]:
-
-
-
 

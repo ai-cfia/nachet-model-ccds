24316359a55380128230f87ef02c3a083f0d0afd
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2025-01-30 08:54:24 -0600

    Update training and evaluation launcher scripts to include new model configurations and comment out unused commands

--- notebooks/3.01_js_eval_launcher.sh
+++ notebooks/3.01_js_eval_launcher.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 screen -dmS "train1" bash -c "./shell/3.02_js_evaluate_6_seed_zoom_120250130.sh"
-screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_6_seed_zoom_220250130.sh"
-screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_6_seed_zoom_320250130.sh"
-
 screen -dmS "train1" bash -c "./shell/3.02_js_evaluate_2_seed_zoom_120250130.sh"
-screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_2_seed_zoom_220250130.sh"
-screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_2_seed_zoom_320250130.sh"
-
-screen -dmS "train1" bash -c "./shell/3.02_js_evaluate_1_seed_zoom_120250130.sh"
-screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_1_seed_zoom_220250130.sh"
-screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_1_seed_zoom_320250130.sh"
-
 screen -dmS "train1" bash -c "./shell/3.02_js_evaluate_27spp_120250130.sh"
-screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_27spp_220250130.sh"
-screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_27spp_320250130.sh"
+
+# screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_6_seed_zoom_220250130.sh"
+# screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_2_seed_zoom_220250130.sh"
+# screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_27spp_220250130.sh"
+
+# screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_6_seed_zoom_320250130.sh"
+# screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_2_seed_zoom_320250130.sh"
+# screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_27spp_320250130.sh"
+
+# screen -dmS "train1" bash -c "./shell/3.02_js_evaluate_1_seed_zoom_120250130.sh"
+# screen -dmS "train2" bash -c "./shell/3.02_js_evaluate_1_seed_zoom_220250130.sh"
+# screen -dmS "train3" bash -c "./shell/3.02_js_evaluate_1_seed_zoom_320250130.sh"

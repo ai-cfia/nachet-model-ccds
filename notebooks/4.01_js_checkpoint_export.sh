08c8e42847ec01f2a1f32ba4bb4ec9c4a3ffbd6d
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2025-02-27 12:43:14 -0600

    Add checkpoint export script for model checkpointing and versioning

---
+++ notebooks/4.01_js_checkpoint_export.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 nachetmodel/CheckpointExporter.py \
+  --checkpoint-path models/27spp_model/model_120250130/checkpoint-9000 \
+  --model_name 27spp_model_1 \
+  --version 1.0
+
+python3 nachetmodel/CheckpointExporter.py \
+  --checkpoint-path models/15spp_zoom_level_validation_models/6_seed_model_120250130/checkpoint-20500 \
+  --model_name 15spp_model_1 \
+  --version 1.0
